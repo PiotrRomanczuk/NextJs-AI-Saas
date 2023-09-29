292f1e80b1b8f9cb98e577cbfab2c31e9fc05dcf
PiotrRomanczuk <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-09-29 20:35:25 +0200

    Finishing the sidebar

--- components/sidebar/mobile-sidebar.tsx
+++ components/sidebar/mobile-sidebar.tsx
@@ -1,13 +1,23 @@
-// "use client"
+'use client';
 
 import { Menu } from 'lucide-react';
-import { Button } from '../ui/button';
+
+import { Button } from '@/components/ui/button';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import Sidebar from '@/components/sidebar/sidebar';
 
 const MobileSidebar = () => {
 	return (
-		<Button value='ghost' size='icon' className='md:hidden'>
-			<Menu />
-		</Button>
+		<Sheet>
+			<SheetTrigger>
+				<Button value='ghost' size='icon' className='md:hidden'>
+					<Menu />
+				</Button>
+			</SheetTrigger>
+			<SheetContent side='left' className='p-0'>
+				<Sidebar />
+			</SheetContent>
+		</Sheet>
 	);
 };
 
